28ed3d76372011c89bc1f8b14a7f655898444c79
Nick Aldridge <naldridge1360@gmail.com>
2021-08-13 11:03:53 -0500

    added slug field to schema and slugify

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE "channels" (
   "id" int PRIMARY KEY,
   "name" varchar(200) NOT NULL,
+  "slug" varchar(200) UNIQUE,
   "description" varchar(500),
   "default_channel" boolean,
   "protected" boolean
