956f90b6a5b831ccfbbccffa39727e31775985dd
Nick Aldridge <naldridge1360@gmail.com>
2021-08-14 16:58:44 -0500

    finished initial pass on PostModel

---
+++ models/PostModel.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const db = require('./conn');
+
+class PostModel {
+    constructor(id, channel_id, title, text_content, mm_content, link_content, likes, user_id) {
+        this.id = id;
+        this.channel_id = channel_id;
+        this.title = title;
+        this.text_content = text_content;
+        this.mm_content = mm_content;
+        this.link_content = link_content;
+        this.likes = likes;
+        this.user_id = user_id;
+    }
+
+    static async getByUser(user_id) {
+        try {
+            const response = await db.any(
+                `SELECT * FROM posts
+                WHERE user_id = '${user_id}';`
+            );
+            return response;
+        } catch (err) {
+            console.error('Error: ', err);
+            return err;
+        }
+    }
+
+    static async getByChannel(channel_id) {
+        try {
+            const response = await db.any(
+                `SELECT * FROM posts
+                WHERE channel_id = '${channel_id}';`
+            );
+            return response;
+        } catch (err) {
+            console.error('Error: ', err);
+            return err;
+        }
+    }
+
+    async addEntry() {
+        try {
+            const response = await db.result(
+                `INSERT INOT posts (channel_id, title, text_content, mm_content, link_content, user_id)
+                VALUES
+                    ('${this.channel_id}', '${this.title}', '${this.text_content}', '${this.mm_content}', '${this.link_content}', '${this.user_id}');`
+            );
+            return response;
+        } catch (err) {
+            console.error('Error: ', err);
+            return err;
+        }
+    }
+}
